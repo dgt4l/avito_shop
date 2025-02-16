4061e34ac55b5a85fa58914ea93dc3f80574b7a6
dgt4l <nik.asiimov@mail.ru>
2025-02-16 03:41:14 +0300

    controller unit tests

--- go.mod
+++ go.mod
@@ -9,10 +9,13 @@ require (
 	github.com/lib/pq v1.10.9
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.19.0
+	github.com/stretchr/testify v1.10.0
+	go.uber.org/mock v0.5.0
 	golang.org/x/crypto v0.31.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
@@ -21,6 +24,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
---
+++ internal/avito_shop/controller/contoller_test.go
@@ -0,0 +1,173 @@
+package controller
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/dgt4l/avito_shop/internal/avito_shop/dto"
+	"github.com/dgt4l/avito_shop/internal/avito_shop/models"
+	"github.com/dgt4l/avito_shop/test/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestShopService_BuyItem(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockRepository(ctrl)
+	mockAuth := mocks.NewMockAuthService(ctrl)
+
+	service := NewShopService(mockRepo, mockAuth, ServiceConfig{Salt: "test-salt"})
+
+	ctx := context.Background()
+	req := &dto.BuyItemRequest{Id: 1, Item: "item1"}
+
+	mockRepo.EXPECT().BuyItem(ctx, req.Id, req.Item).Return(nil)
+
+	err := service.BuyItem(ctx, req)
+	assert.NoError(t, err)
+}
+
+func TestShopService_GetInfo(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockRepository(ctrl)
+	mockAuth := mocks.NewMockAuthService(ctrl)
+
+	service := NewShopService(mockRepo, mockAuth, ServiceConfig{Salt: "test-salt"})
+
+	ctx := context.Background()
+	userId := 1
+	expectedInfo := &dto.InfoResponse{
+		Coins: 100,
+		Inventory: []dto.Inventory{
+			{Type: "item1", Quantity: 1},
+		},
+		CoinHistory: dto.CoinHistory{
+			Received: []dto.Received{
+				{FromUser: "user2", Amount: 50},
+			},
+			Sent: []dto.Sent{
+				{ToUser: "user3", Amount: 30},
+			},
+		},
+	}
+
+	mockRepo.EXPECT().GetInfo(ctx, userId).Return(expectedInfo, nil)
+
+	info, err := service.GetInfo(ctx, userId)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedInfo, info)
+}
+
+func TestShopService_SendCoin(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockRepository(ctrl)
+	mockAuth := mocks.NewMockAuthService(ctrl)
+
+	service := NewShopService(mockRepo, mockAuth, ServiceConfig{Salt: "test-salt"})
+
+	ctx := context.Background()
+	fromUserId := 1
+	req := &dto.SendCoinRequest{ToUser: "user2", Amount: 50}
+
+	mockRepo.EXPECT().SendCoin(ctx, req.ToUser, fromUserId, req.Amount).Return(nil)
+
+	err := service.SendCoin(ctx, fromUserId, req)
+	assert.NoError(t, err)
+}
+
+func TestShopService_CreateUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockRepository(ctrl)
+	mockAuth := mocks.NewMockAuthService(ctrl)
+
+	service := NewShopService(mockRepo, mockAuth, ServiceConfig{Salt: "test-salt"})
+
+	ctx := context.Background()
+	req := &dto.AuthRequest{Username: "user1", Password: "password1"}
+	expectedUser := &models.User{Id: 1, Username: "user1", Password: "password1"}
+
+	mockRepo.EXPECT().CreateUser(ctx, req.Username, gomock.Any()).Return(1, nil)
+
+	user, err := service.CreateUser(ctx, req)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedUser.Id, user.Id)
+	assert.Equal(t, expectedUser.Username, user.Username)
+	assert.Equal(t, expectedUser.Password, user.Password)
+}
+
+func TestShopService_AuthUser_NewUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockRepository(ctrl)
+	mockAuth := mocks.NewMockAuthService(ctrl)
+
+	service := NewShopService(mockRepo, mockAuth, ServiceConfig{Salt: "test-salt"})
+
+	ctx := context.Background()
+	req := &dto.AuthRequest{Username: "user1", Password: "password1"}
+	expectedUser := &models.User{Id: 1, Username: "user1", Password: "password1"}
+	expectedToken := "test-token"
+
+	mockRepo.EXPECT().GetUser(ctx, req.Username).Return(nil, errors.New("user not found"))
+	mockRepo.EXPECT().CreateUser(ctx, req.Username, gomock.Any()).Return(1, nil)
+	mockAuth.EXPECT().GenerateToken(expectedUser).Return(expectedToken, nil)
+
+	authResponse, err := service.AuthUser(ctx, req)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedToken, authResponse.Token)
+}
+
+func TestShopService_AuthUser_ExistingUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockRepository(ctrl)
+	mockAuth := mocks.NewMockAuthService(ctrl)
+
+	service := NewShopService(mockRepo, mockAuth, ServiceConfig{Salt: "test-salt"})
+
+	ctx := context.Background()
+	req := &dto.AuthRequest{Username: "user1", Password: "password1"}
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("password1test-salt"), bcrypt.DefaultCost)
+	existingUser := &models.User{Id: 1, Username: "user1", Password: string(hashedPassword)}
+	expectedToken := "test-token"
+
+	mockRepo.EXPECT().GetUser(ctx, req.Username).Return(existingUser, nil)
+	mockAuth.EXPECT().GenerateToken(existingUser).Return(expectedToken, nil)
+
+	authResponse, err := service.AuthUser(ctx, req)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedToken, authResponse.Token)
+}
+
+func TestShopService_AuthUser_InvalidPassword(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockRepository(ctrl)
+	mockAuth := mocks.NewMockAuthService(ctrl)
+
+	service := NewShopService(mockRepo, mockAuth, ServiceConfig{Salt: "test-salt"})
+
+	ctx := context.Background()
+	req := &dto.AuthRequest{Username: "user1", Password: "wrong-password"}
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte("password1test-salt"), bcrypt.DefaultCost)
+	existingUser := &models.User{Id: 1, Username: "user1", Password: string(hashedPassword)}
+
+	mockRepo.EXPECT().GetUser(ctx, req.Username).Return(existingUser, nil)
+
+	_, err := service.AuthUser(ctx, req)
+	assert.Error(t, err)
+	assert.Equal(t, ErrInvalidPasswd, err)
+}
